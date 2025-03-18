4e2cdabc6a204f482682b2a2b769d7d21222bfff
IsmaGuerra <iguerrae@gmail.com>
2025-03-18 02:19:18 -0300

    agregando  nombre al arreglo con metodo solicitado Push

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos =[];
 
-//agrega el nombre al arreglo con metodo Push.
+//agregando  nombre al arreglo con metodo solicitado Push
 
 function agregarAmigo() {
     const inputAmigo = document.getElementById('amigo');
